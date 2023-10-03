c33601d1bf9f00358fda80e87dd7d195a85f156c
nguyenpatz <nguyenpatzz@gmail.com>
2023-10-03 22:57:03 +0700

    making landing page

---
+++ src/components/BodyHeader1.js
@@ -0,0 +1,9 @@
+import '../assets/scss/BodyHeader.scss';
+
+export default function BodyHeader1() {
+    return (
+        <div className="body-header">
+            
+        </div>
+    );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import "../assets/scss/Header.scss";
+import Logo from "../assets/images/youth_sport_logo.jpeg";
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-1">
+        <a href="#">
+          <img src={Logo} className="header-logo" alt="Logo" />
+        </a>
+      </div>
+      <div className="header-2">
+        <div>About</div>
+        <div>Event</div>
+        <div>Blog</div>
+        <div>Services</div>
+      </div>
+      <div className="header-3">
+        <div>Contact</div>
+      </div>
+    </header>
+  );
+}
